009f764acbe9e31cea985c1793230b4d79f30dd9
DaniHDZM <sayu142184@gmail.com>
2025-09-23 16:26:44 -0600

    Lista de tareas

---
+++ src/components/ListaTareas.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <h2>Lista de tareas</h2>
+        <input type="text" v-model="nuevaTarea" @keyup.enter="añadirTarea">
+        <ul>
+            <li v-for="(tarea, index) in tareas" key="index">
+                {{ tarea }}
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script setup>
+    import { ref } from 'vue';
+    const tareas = ref([]);
+    const nuevaTarea = ref('');
+    const añadirTarea = () => {
+        tareas.value.push(nuevaTarea.value);
+        nuevaTarea.value = '';
+    }
+</script>
+
+<style scoped>
+
+</style>
